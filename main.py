f1259507d44bb9ec766779c3ece46afdb73ac108
Marina <marinabar1609@gmail.com>
2021-09-20 09:15:29 +0200

    1try

--- main.py
+++ main.py
@@ -4,3 +4,12 @@
 # sauvegarder infos dans le fichier mp3, ajouter cover art
 
 #pour les playlists, utiliser spotifyscraper pour obtenir les titres, et faire une boucle
+
+#1
+from SpotifyScraper.scraper import Scraper, Request
+
+request = Request().request()
+scraper=Scraper(session=request)
+track_information = scraper.get_track_url_info(url="http://open.spotify.com/embed/track/6txVOdSbg928oeGhlVUrdK")
+
+print(track_information)
