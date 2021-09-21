bef73b046e824247ebaee3b839932217d96b7032
Marina <marinabar1609@gmail.com>
2021-09-21 18:09:23 +0200

    default save

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
 print("https://www.youtube.com/watch?v=" + video_ids[0])
 
 
-#print(playlist['tracks_list'][0]['track_name']) https://open.spotify.com/playlist/6iV0C1GGfNmkxH7M3qddl5
+#https://open.spotify.com/playlist/6iV0C1GGfNmkxH7M3qddl5
