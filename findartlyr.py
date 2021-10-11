4b8ed26e6932d84d6796b354b1e591d17843aefd
Marina <marinabar1609@gmail.com>
2021-10-11 20:16:36 +0200

    trying re and requests - not 100%

--- findartlyr.py
+++ findartlyr.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 import time
 import lxml
 from urllib.request import urlopen
@@ -11,13 +12,12 @@ headers = {
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
 }
 
-#urlbase='https://www.google.fr/search?q=bubble+tea&tbm=isch'
 
 
 def findart(titres):
     urlbase= titres[0][5]
     html = requests.get(urlbase)
-    soup = BeautifulSoup(html.content, 'lxml')
+    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode()) #NOT CORRECT
     el = soup.findAll(class_="style-scope yt-img-shadow")
     print(el)
 
