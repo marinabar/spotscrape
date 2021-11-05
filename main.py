70684faf7414c5d301db1d6bf09d90adb0466586
Marina <marinabar1609@gmail.com>
2021-11-05 20:00:30 +0100

    get art with request-but img format not 100% right

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-#à faire
-# avec Spotify scraper, trouver titre et artiste et cover art, tire album, durée
-# télécherger mp3 à partir de recherche sur youtube en 'audio' ssi mp3<durée +10s
-# sauvegarder infos dans le fichier mp3, ajouter cover art
-
-#pour les playlists, utiliser spotifyscraper pour obtenir les titres, et faire une boucle
-
-#1
-
 from remover import remover
 
 import urllib.request
